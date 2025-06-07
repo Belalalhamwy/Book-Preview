4043a2918b816c257e0fb65d3c4fd5dd9a2e45aa
belal alhamwy <belalalhamwy@gmail.com>
2025-06-08 00:32:07 +0300

    change num

--- lib/constantse.dart
+++ lib/constantse.dart
@@ -3,6 +3,6 @@
 import 'package:flutter/material.dart';
 
 Color Kprimarycolor = const Color.fromARGB(255, 21, 4, 37);
-const KTranstionDuration = Duration(microseconds: 2500);
+const KTranstionDuration = Duration(microseconds: 2501);
 const KTranstionDuration2 = Duration(microseconds: 5000);
 const KGtSectraFine = "Playfair_Display";
